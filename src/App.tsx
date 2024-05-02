4ed8b2b34ad38e18d36a08f69a916fe8b2e5c2f8
Tung Dinh <tungxd301@gmail.com>
2024-05-01 19:01:28 -0700

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Layout, theme } from "antd";
+import { Content } from "antd/es/layout/layout";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import { LandingPage } from "./pages/LandingPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 function App() {
+  const Wrapper = () => {
+    const { token } = theme.useToken();
+    return (
+      <Layout style={{ minHeight: "100vh" }}>
+        <Content
+          style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 280,
+            background: token.colorBgContainer,
+            borderRadius: token.borderRadiusLG,
+          }}
+        >
+          <Outlet />
+        </Content>
+      </Layout>
+    );
+  };
+
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: <LandingPage />,
+      element: <Wrapper />,
+      errorElement: <NotFoundPage />,
+      children: [
+        {
+          path: "/",
+          element: <LandingPage />,
+        },
+      ],
     },
   ]);
 
