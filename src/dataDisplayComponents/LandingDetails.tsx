3412e19ed59df106857bfd25153b42c6ace69d81
Tung Dinh <tungxd301@gmail.com>
2024-05-04 15:19:02 -0700

    Changes

--- src/dataDisplayComponents/LandingDetails.tsx
+++ src/dataDisplayComponents/LandingDetails.tsx
@@ -1,4 +1,4 @@
-import { Flex, Typography } from "antd";
+import { Flex, Image, Typography } from "antd";
 import { lightColor } from "../config/layoutConfig";
 
 export const LandingDetails = () => {
@@ -6,13 +6,36 @@ export const LandingDetails = () => {
     <Flex
       style={{ backgroundColor: lightColor, padding: "128px 32px" }}
       justify="center"
+      align="center"
+      gap={32}
     >
-      <Typography.Title
-        level={2}
-        style={{ margin: 0, textAlign: "center", width: "40%" }}
-      >
-        Speed
-      </Typography.Title>
+      <Flex vertical gap={32} flex={1}>
+        <Typography.Title level={2} style={{ margin: 0 }}>
+          Simple integration
+        </Typography.Title>
+        <Typography.Title level={4} style={{ margin: 0 }}>
+          Integration
+        </Typography.Title>
+        <Typography.Paragraph>
+          Experience effortless integration with our platform, enabling you to
+          kickstart asynchronous tasks seamlessly through a single, synchronized
+          API call
+        </Typography.Paragraph>
+        <Typography.Title level={4} style={{ margin: 0 }}>
+          Time saving
+        </Typography.Title>
+        <Typography.Paragraph>
+          Simplify your workflow and save valuable time with our intuitive
+          solution, designed to streamline your processes and enhance efficiency
+        </Typography.Paragraph>
+      </Flex>
+
+      <Flex flex={1}>
+        <Image
+          src="https://tcw-images.s3.us-west-2.amazonaws.com/simple-integration.png"
+          preview={false}
+        />
+      </Flex>
     </Flex>
   );
 };
