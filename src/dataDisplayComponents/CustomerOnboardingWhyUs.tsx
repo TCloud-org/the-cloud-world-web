b074f8596c8baed33ab6044e18c0cb89bd1858c6
Tung Dinh <tungxd301@gmail.com>
2024-05-18 18:19:23 -0700

    Changes

--- src/dataDisplayComponents/CustomerOnboardingWhyUs.tsx
+++ src/dataDisplayComponents/CustomerOnboardingWhyUs.tsx
@@ -18,10 +18,29 @@ export const CustomerOnboardingWhyUs = () => {
           title="Why us"
           subtitle="Make customer onboarding process faster and simpler"
         />
+
         <LandingInfo
           section="Win more customers"
           title="Craft exceptional first impressions for your customers"
           description="In today's competitive landscape, the initial impression your business leaves can determine whether you gain a lifelong customer or lose them to a competitor. With our workflow solutions, you can establish a robust process for setting up your customers, ensuring that each interaction leaves a lasting positive impression."
+          demonstration={
+            <WorkflowMockup
+              steps={[
+                "Start",
+                "SetupCustomerAccount",
+                "SetupCustomerBillingInformation",
+                "SendGreetingMessageToCustomer",
+                "End",
+              ]}
+            />
+          }
+        />
+
+        <LandingInfo
+          section="Win more customers"
+          title="Craft exceptional first impressions for your customers"
+          description="In today's competitive landscape, the initial impression your business leaves can determine whether you gain a lifelong customer or lose them to a competitor. With our workflow solutions, you can establish a robust process for setting up your customers, ensuring that each interaction leaves a lasting positive impression."
+          position="right"
           demonstration={
             <WorkflowMockup
               steps={[
