acdcf26eb84d86478c440f858323d520fb98f75e
Tung Dinh <tungxd301@gmail.com>
2024-06-12 14:08:01 -0700

    Changes

--- src/config/amsEndpointConfig.tsx
+++ src/config/amsEndpointConfig.tsx
@@ -1,4 +1,3 @@
-export const AMS_BASE_URL =
-  "https://ams-elb-2009189905.us-west-2.elb.amazonaws.com/api/v1";
+export const AMS_BASE_URL = "https://ams.be.thecloudworlds.com/api/v1";
 export const AMS_SUBSCRIBE_NEWSLETTER_ENDPOINT =
   AMS_BASE_URL + "/subscribe-newsletter";
--- src/config/wosEndpointConfig.tsx
+++ src/config/wosEndpointConfig.tsx
@@ -1,3 +1,2 @@
-export const wosBaseUrl =
-  "https://wos-server-142456886.us-west-2.elb.amazonaws.com/api/private/v1";
+export const wosBaseUrl = "https://wos.be.thecloudworlds.com/api/private/v1";
 export const wosContactMe = wosBaseUrl + "/contact-me";
